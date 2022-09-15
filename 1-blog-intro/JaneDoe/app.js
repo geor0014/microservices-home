4cec29195280988a3ec9034ec3932499e244fa73
georgiev098 <geor0014@hz.nl>
2022-09-15 12:28:50 +0200

    add bubbleSort to JaneDoe.js

--- 1-blog-intro/JaneDoe/app.js
+++ 1-blog-intro/JaneDoe/app.js
@@ -3,3 +3,16 @@ console.log('Listening on 4005')
 function calculateSum(a,b) {
     return a + b
 }
+
+function bubbleSort (array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] > array[j+1]) {
+                let temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+            }
+        }
+    }
+    return array
+}
