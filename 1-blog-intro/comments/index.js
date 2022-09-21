a08529df981a74a8c159869b2663a3f6a35c534a
georgiev098 <geor0014@hz.nl>
2022-09-21 21:51:00 +0200

    add moderation function and event sync

--- 1-blog-intro/comments/index.js
+++ 1-blog-intro/comments/index.js
@@ -25,20 +25,46 @@ app.post('/posts/:id/comments', async(req,res)=>{
     // if the post id is not in the commentsByPostId object, give an empty array
     const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({id:commentId, content});
+    comments.push({id:commentId, content, status:'pending'});
 
     commentsByPostId[req.params.id] = comments;
 
    await axios.post('http://localhost:4005/events',{
         type: "CommentCreated",
-        data: {id:commentId,content, postId: req.params.id}
+        data: {id:commentId,content, postId: req.params.id, status:'pending'}
     })
 
     res.status(201).send(comments);
 })
 
-app.post('/events',(req,res)=>{
+app.post('/events',async (req,res)=>{
     console.log('Received Event', req.body.type);
+
+    const {type, data} = req.body;
+
+    if(type === 'CommentModerated'){
+        const {postId,id,status,content} = data;
+        const comments = commentsByPostId[postId];
+
+        // find the comment from the comments array
+        const comment = comments.find(comment =>{
+            // return the comment with the id that matches the id from the data object
+            return comment.id === id;
+        })
+        // change the status of the comment to the status from the data object
+        comment.status = status;
+
+        // send an event to the event bus
+        axios.post('http://localhost:4005/events',{
+            type: 'CommentUpdated',
+            data: {
+                id,
+                status,
+                postId,
+                content
+            }
+        })
+    }
     res.send({});
 })
 
--- 1-blog-intro/query/index.js
+++ 1-blog-intro/query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,6 +9,36 @@ app.use(cors());
 
 // create an empty object to store the posts
 const posts = {};
+
+const handleEvent = (type, data) => {
+    if(type === 'PostCreated'){
+        // get the id and title of the post from the data object
+        const {id, title} = data;
+        // add the post to the posts object
+        posts[id] = {id,title, comments: []};
+    }
+
+    if( type === 'CommentCreated'){
+        // get the id, content, and postId from the data object
+        const {id,content,postId, status} = data
+        // get the post from the posts object with the postId
+        const post = posts[postId];
+        // add the comment to the post's comments array 
+        post.comments.push({id,content,status});
+    }
+
+    if(type === 'CommentUpdated'){
+        const {id, content, postId, status} = data;
+
+        const post = posts[postId];
+        const comment = post.comments.find(comment =>{
+            return comment.id === id;
+        })
+
+        comment.status = status;
+        comment.content = content;
+    }
+}
 
 app.get('/posts',(req,res)=>{
 
@@ -19,27 +50,20 @@ app.get('/posts',(req,res)=>{
 app.post('/events',(req,res)=>{
     const {type, data} = req.body;
 
-    if(type === 'PostCreated'){
-        // get the id and title of the post from the data object
-        const {id, title} = data;
-        // add the post to the posts object
-        posts[id] = {id,title, comments: []};
-    }
-
-    if( type === 'CommentCreated'){
-        // get the id, content, and postId from the data object
-        const {id,content,postId} = data
-        // get the post from the posts object with the postId
-        const post = posts[postId];
-        // add the comment to the post's comments array 
-        post.comments.push({id,content});
-    }
-
-    console.log(posts);
+    handleEvent(type,data);
 
     res.send({});
 })
 
-app.listen(4002,()=>{
+app.listen(4002, async ()=>{
     console.log('Listening on 4002');
+
+    const res = await axios.get(`http://localhost:4005/events`);
+
+
+    res.data.forEach(event => {
+        console.log('Processing event:', event.type);
+
+        handleEvent(event.type, event.data)
+    });
 })
